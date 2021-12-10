ddec33be69d45589e8922f80b4a267b7c11744de
tianxuanzhizi <286129936@qq.com>
2021-12-10 18:03:12 +0800

    :tada:

---
+++ src/mouseEffect.vue
@@ -0,0 +1,12 @@
+<template>
+     <canvas class="fireworks"
+        style="position: fixed; left: 0; top: 0; z-index: 99999999; pointer-events: none;"></canvas>
+</template>
+<script>
+import init from './mouseEffect';
+export default {
+    mounted(){
+        init();
+    }
+}
+</script>
